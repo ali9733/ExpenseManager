c477c273dcc1f4c8e8cca8820eddf0be05974f29
alamin <alamin@gmail.com>
2025-09-12 12:11:09 +0530

    add dialog fragmrnt

---
+++ app/src/main/java/com/alaminali/expensemanager/fragmentPackage/dialogFragment.java
@@ -0,0 +1,40 @@
+package com.alaminali.expensemanager.fragmentPackage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alaminali.expensemanager.R;
+
+public class dialogFragment extends Fragment
+{
+
+
+
+    public dialogFragment()
+    {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+
+        return inflater.inflate(R.layout.fragment_dialog, container, false);
+    }
+
+
+}
