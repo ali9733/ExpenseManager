7e5daa7f2590b195d19e5d80cbc87505d32cad0c
alamin <alamin@gmail.com>
2025-09-12 11:52:39 +0530

    in this commit add tablayout linearlayout for income and expense also add recyclerview in transction fragment

--- app/src/main/java/com/alaminali/expensemanager/fragmentPackage/statsFragment.java
+++ app/src/main/java/com/alaminali/expensemanager/fragmentPackage/statsFragment.java
@@ -9,6 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.alaminali.expensemanager.R;
+import com.alaminali.expensemanager.databinding.FragmentStatsBinding;
+import com.anychart.AnyChart;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class statsFragment extends Fragment {
 
@@ -27,11 +38,44 @@ public class statsFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
     }
-
+     FragmentStatsBinding statsBinding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        statsBinding=FragmentStatsBinding.inflate(inflater,container,false);
 
-        return inflater.inflate(R.layout.fragment_stats, container, false);
+        /* SETTING PIE CHART ON STATSICTICS FRAGMENT BELOW */
+
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();                  // INSERT HERE CUSTOM DATA TO SHOW CHART
+        data.add(new ValueDataEntry("Apples", 6371664));
+        data.add(new ValueDataEntry("Pears", 789622));
+        data.add(new ValueDataEntry("Bananas", 7216301));
+        data.add(new ValueDataEntry("Grapes", 1486621));
+        data.add(new ValueDataEntry("Oranges", 1200000));
+
+        pie.data(data);
+
+        pie.title("Fruits imported in 2015 (in kg)");
+
+        pie.labels().position("outside");
+
+        pie.legend().title().enabled(true);
+        pie.legend().title()
+                .text("Retail channels")
+                .padding(0d, 0d, 10d, 0d);
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+
+        statsBinding.anyChartView.setChart(pie);
+
+
+        //------------END CODE HERE-------------
+
+        return statsBinding.getRoot();
     }
 }
